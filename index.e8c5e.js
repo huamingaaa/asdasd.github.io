97ef83c8c3041bc831a8d9eee091ddfdb70c0531
chen <704261009@qq.com>
2023-03-06 17:42:47 +0800

    2023年3月6日17:42:32

--- index.e8c5e.js
+++ index.e8c5e.js
@@ -1,4 +1,4 @@
-System.register(["./application.4eed5.js"], function (_export, _context) {
+System.register(["./application.64c93.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.4eed5.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application4eed5Js) {
-      createApplication = _application4eed5Js.createApplication;
+    setters: [function (_application64c93Js) {
+      createApplication = _application64c93Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
